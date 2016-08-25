49a655663b728ba961db46f5d9217948fadbb620
SuicideHideout <andriux1233@gmail.com>
2016-08-26 00:13:46 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -99,7 +99,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/basicBot/source/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/SuicideHideout/saltinis/master/lang/Langindex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
